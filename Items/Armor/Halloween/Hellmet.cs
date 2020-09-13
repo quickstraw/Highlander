08035666b9cdd8f174364b2c34e34e61825c8970
Nathan McGraw <nathanmcgraw64@gmail.com>
2020-09-12 22:48:34 -0500

    Added some Halloween hats and updated PyromancerMask and ToySoldier

---
+++ Items/Armor/Halloween/Hellmet.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using static Terraria.ModLoader.ModContent;
+
+namespace Highlander.Items.Armor.Halloween
+{
+    [AutoloadEquip(EquipType.Head)]
+    class Hellmet : AbnormalItem
+    {
+        public Hellmet() : base()
+        {
+        }
+        public Hellmet(AbnormalEffect effect) : base(effect)
+        {
+        }
+        public override string Texture => "Highlander/Items/Armor/Halloween/Hellmet";
+
+        public override void SetStaticDefaults()
+        {
+            //Tooltip.SetDefault("Eastern designed helmet that looks eerily like a lid.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = 3;
+            item.vanity = true;
+            base.SetDefaults();
+        }
+
+        public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+        {
+            drawAltHair = true;
+        }
+
+    }
+}
