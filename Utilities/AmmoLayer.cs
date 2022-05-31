b685966b6f608d42c740509c9d00d4edfb931019
Nathan McGraw <nathanmcgraw64@gmail.com>
2022-05-31 18:28:03 -0500

    Added compact gun UI and client UI options.

--- Utilities/AmmoLayer.cs
+++ Utilities/AmmoLayer.cs
@@ -39,6 +39,7 @@ namespace Highlander.Utilities
             Player drawPlayer = drawInfo.drawPlayer;
             HighlanderPlayer modPlayer = drawPlayer.GetModPlayer<HighlanderPlayer>();
             Mod mod = Highlander.Instance;
+            bool altTextures = ModContent.GetInstance<HighlanderClientConfig>().AlternateGunUI;
 
             if (drawInfo.shadow != 0f)
             {
@@ -87,28 +88,84 @@ namespace Highlander.Utilities
                 switch (modPlayer.currentAmmo)
                 {
                     case 0:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo06";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo06";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact06";
+                        }
                         break;
                     case 1:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo16";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo16";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact16";
+                        }
                         break;
                     case 2:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo26";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo26";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact26";
+                        }
                         break;
                     case 3:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo36";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo36";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact36";
+                        }
                         break;
                     case 4:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo46";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo46";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact46";
+                        }
                         break;
                     case 5:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo56";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo56";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact56";
+                        }
                         break;
                     case 6:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo66";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo66";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact66";
+                        }
                         break;
                     default:
-                        texPath = "Highlander/UI/GunAmmo6/GunAmmo06";
+                        if (altTextures)
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/GunAmmo06";
+                        }
+                        else
+                        {
+                            texPath = "Highlander/UI/GunAmmo6/Compact06";
+                        }
                         break;
                 }
                 yOffset -= 6;
@@ -123,13 +180,22 @@ namespace Highlander.Utilities
 
         public static DrawData AmmoGunDrawData(PlayerDrawSet drawInfo, string ammoCounterSprite, int yOffset, float angleInRadians)
         {
+            bool altTextures = ModContent.GetInstance<HighlanderClientConfig>().AlternateGunUI;
+            int ammoPosY = 80;
+            if (altTextures)
+            {
+                ammoPosY = 60;
+            }
+            
+            float opacity = 0.9f;
+
             Player drawPlayer = drawInfo.drawPlayer;
             Mod mod = Highlander.Instance;
             HighlanderPlayer modPlayer = drawPlayer.GetModPlayer<HighlanderPlayer>();
             float scale = 1f;
             Texture2D texture = Request<Texture2D>(ammoCounterSprite).Value;
             int drawX = (int)(drawInfo.Position.X + drawPlayer.width / 2f - Main.screenPosition.X);
-            int drawY = (int)(drawInfo.Position.Y + yOffset + 70 - Main.screenPosition.Y);
+            int drawY = (int)(drawInfo.Position.Y + yOffset + ammoPosY - Main.screenPosition.Y);
 
             if (drawPlayer.mount.Active)
             {
@@ -143,10 +209,9 @@ namespace Highlander.Utilities
                 int smoothOffset = (int)((drawInfo.Position - Main.screenPosition) - (drawPlayer.position - Main.screenPosition)).Y + data.heightBoost;
 
                 drawX = (int)(pos.X + drawPlayer.width / 2f - Main.screenPosition.X);
-                drawY = (int)(pos.Y + yOffset - smoothOffset + 70 - Main.screenPosition.Y);
+                drawY = (int)(pos.Y + yOffset - smoothOffset + ammoPosY - Main.screenPosition.Y);
             }
-
-            return new DrawData(texture, new Vector2(drawX, drawY), new Rectangle(0, 0, texture.Width, texture.Height), Color.White, angleInRadians, new Vector2(texture.Width / 2f, texture.Height / 2f), scale, SpriteEffects.None, 0);
+            return new DrawData(texture, new Vector2(drawX, drawY), new Rectangle(0, 0, texture.Width, texture.Height), Color.White * opacity, angleInRadians, new Vector2(texture.Width / 2f, texture.Height / 2f), scale, SpriteEffects.None, 0);
         }
     }
 }
