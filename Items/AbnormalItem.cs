7a6f2f71e42bf06c9529a1d11a4da24ffc0d06ff
Nathan McGraw <nathanmcgraw64@gmail.com>
2022-06-01 23:10:06 -0500

    Fixed AbnormalItems.

--- Items/AbnormalItem.cs
+++ Items/AbnormalItem.cs
@@ -17,13 +17,15 @@ namespace Highlander.Items
     class AbnormalItem : ModItem
     {
         public override string Name => CurrentEffect != 0 ? "Unusual" + GetType().Name : GetType().Name;
-        public AbnormalEffect CurrentEffect { get; set; }
+        public AbnormalEffect CurrentEffect = AbnormalEffect.Unknown;
         public int counter = 0;
         public List<AbnormalEffect> Table = RollTable.AbnormalRollTable.Table;
         private bool roll = true;
 
         //Can't use [Autoload(false)] lest deriving types not get added
         public sealed override bool IsLoadingEnabled(Mod mod) => SafeIsLoadingEnabled(mod) ?? false;
+
+        protected override bool CloneNewInstances => true;
 
         /// <summary>
         /// Allows you to safely request whether this item should be autoloaded
@@ -35,7 +37,8 @@ namespace Highlander.Items
         public override void SetDefaults()
         {
             Item.accessory = true;
-            if (roll) {
+            if (roll)
+            {
                 CurrentEffect = ReturnRollAbnormalEffect();
                 Item.rare = ItemRarityID.LightPurple;
             }
@@ -122,132 +125,6 @@ namespace Highlander.Items
 
         protected void PlayAbnormalEffect(Player player)
         {
-            /**Vector2 headPosition;
-            float headHeight;
-            Dust currDust;
-            ModDustCustomData data;
-
-            switch (CurrentEffect)
-            {
-                case AbnormalEffect.Unknown:
-                    break;
-                case AbnormalEffect.None:
-                    break;
-                case AbnormalEffect.PurpleEnergy:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight - 14;
-                    headPosition.X -= 6 - 3;
-
-                    currDust = Dust.NewDustPerfect(headPosition, mod.DustType("PurpleEnergy"));
-                    data = new ModDustCustomData(player);
-                    currDust.customData = data;
-                    break;
-                case AbnormalEffect.GreenEnergy:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight - 14;
-                    headPosition.X -= 6 - 3;
-
-                    currDust = Dust.NewDustPerfect(headPosition, mod.DustType("GreenEnergy"));
-                    data = new ModDustCustomData(player);
-                    currDust.customData = data;
-                    break;
-                case AbnormalEffect.BurningFlames:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight + 16;
-                    headPosition.X -= player.width / 2 + 2;
-
-                    currDust = Dust.NewDustDirect(headPosition, player.width, player.height / 8 + 4, mod.DustType("BurningFlames"));
-                    data = new ModDustCustomData(player);
-                    currDust.customData = data;
-                    break;
-                case AbnormalEffect.ScorchingFlames:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight + 16;
-                    headPosition.X -= player.width / 2 + 2;
-
-                    currDust = Dust.NewDustDirect(headPosition, player.width, player.height / 8 + 4, mod.DustType("ScorchingFlames"));
-                    data = new ModDustCustomData(player);
-                    currDust.customData = data;
-                    break;
-                case AbnormalEffect.BlizzardyStorm:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight + 28;
-                    headPosition.X -= 2 * player.width / 3 - 4;
-
-                    if (counter % 5 == 0)
-                    {
-                        currDust = Dust.NewDustDirect(headPosition, player.width / 3, player.height / 8, mod.DustType("BlizzardyStorm"));
-                        data = new ModDustCustomData(player);
-                        currDust.customData = data;
-                    }
-                    else if (counter % 4 == 0)
-                    {
-                        headPosition.X += 0;
-                        headPosition.Y += 12;
-                        currDust = Dust.NewDustDirect(headPosition, player.width, player.height / 8, mod.DustType("BlizzardyStormParticle"));
-                        data = new ModDustCustomData(player);
-                        currDust.customData = data;
-                    }
-
-                    counter = (counter + 1) % 60;
-                    break;
-                case AbnormalEffect.StormyStorm:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight + 28;
-                    headPosition.X -= 2 * player.width / 3 - 4;
-
-                    if (counter % 5 == 0)
-                    {
-                        currDust = Dust.NewDustDirect(headPosition, player.width / 3, player.height / 8, mod.DustType("StormyStorm"));
-                        data = new ModDustCustomData(player);
-                        currDust.customData = data;
-                    }
-                    else if (counter % 4 == 0)
-                    {
-                        headPosition.X += 0;
-                        headPosition.Y += 12;
-                        currDust = Dust.NewDustDirect(headPosition, player.width, player.height / 8, mod.DustType("StormyStormParticle"));
-                        data = new ModDustCustomData(player);
-                        currDust.customData = data;
-                    }
-
-                    counter = (counter + 1) % 60;
-                    break;
-                case AbnormalEffect.Cloud9:
-                    headPosition = player.Center;
-                    headHeight = 2 * (player.height / 5);
-                    headPosition.Y -= headHeight + 12;
-                    headPosition.X -= player.width / 2 + 8;
-
-                    if (counter % 30 == 0)
-                    {
-                        currDust = Dust.NewDustDirect(headPosition, player.width + 16, player.height / 8 + 10, mod.DustType("Cloud9"));
-                        data = new ModDustCustomData(player);
-                        currDust.customData = data;
-                        var trailDust = Dust.NewDustPerfect(currDust.position - currDust.velocity, mod.DustType("Cloud9Trail"), currDust.velocity * 0.5f);
-                        data = new ModDustCustomData(player);
-                        trailDust.customData = data;
-                        trailDust.scale = 0.8f;
-                        trailDust = Dust.NewDustPerfect(currDust.position - currDust.velocity, mod.DustType("Cloud9Trail"), currDust.velocity * 0.25f);
-                        data = new ModDustCustomData(player);
-                        trailDust.customData = data;
-                        trailDust.scale = 0.4f;
-                        trailDust = Dust.NewDustPerfect(currDust.position - currDust.velocity, mod.DustType("Cloud9Trail"), currDust.velocity * 0.125f);
-                        data = new ModDustCustomData(player);
-                        trailDust.customData = data;
-                        trailDust.scale = 0.2f;
-                    }
-                    counter = (counter + 1) % 60;
-                    break;
-                default:
-                    break;
-            }**/
         }
     }
 }
--- Sounds/Custom/HighlanderSounds.cs
+++ Sounds/Custom/HighlanderSounds.cs
@@ -10,21 +10,21 @@ namespace Highlander.Sounds.Custom
 {
     public class HighlanderSounds
     {
-        public static readonly SoundStyle ThunderSound = new($"{nameof(Highlander)}/Sounds/Custom/Thunder", 3)
+        public static readonly SoundStyle ThunderSound = new($"{nameof(Highlander)}/Sounds/Custom/Thunder", 1)
         {
             Volume = 0.12f,
             Pitch = 0.2f,
             PitchVariance = 0.1f
         };
 
-        public static readonly SoundStyle UnusualOpenSound = new($"{nameof(Highlander)}/Sounds/Custom/UnusualOpen", 3)
+        public static readonly SoundStyle UnusualOpenSound = new($"{nameof(Highlander)}/Sounds/Custom/UnusualOpen", 1)
         {
             Volume = 1.0f,
             Pitch = -1.0f,
             PitchVariance = 0.1f
         };
 
-        public static readonly SoundStyle UnusualPop = new($"{nameof(Highlander)}/Sounds/Custom/UnusualPop", 3)
+        public static readonly SoundStyle UnusualPop = new($"{nameof(Highlander)}/Sounds/Custom/UnusualPop", 1)
         {
             Volume = 0.7f,
             Pitch = -1.0f,
